91677a1889d6c7fba30d36f3439e94f0f76e0caa
Sebrskaya <114391597+Sebrskaya@users.noreply.github.com>
2023-10-18 16:26:44 +0500

    Update RecentList.cs

--- NoteBookProg/RecentList.cs
+++ NoteBookProg/RecentList.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace NoteBookProg
 {
@@ -14,31 +15,46 @@ namespace NoteBookProg
         public new void Add(String Path)
         {
             
-                if (this.Count < 5 )
+                if (this.Count < 5)
                 {
-                base.Add(Path);
-                RecentListAddNotify?.Invoke();
+                    base.Insert(0, Path);
+                    RecentListAddNotify?.Invoke();
                 
                 }
                 else
                 {
-
+                    base.RemoveAt(4);
+                    base.Insert(0, Path);
                 }
+            }
             
             
             //макс 5 элементов листе
             //если мы открываем файл и он уже есть в списке, то мы его поднимаем на верх 
             
-
-        }
         public void SaveData()
         {
             //записать элементы листа в файл
-
+            Stream stream = new FileStream("Path", FileMode.Create);
+            byte[] buffer = Encoding.Default.GetBytes(reciveTextBox.Text);
+            stream.Write(buffer, 0, buffer.Length);
+            stream.Close();
+            reciveTextBox.Modified = false;
         }
         public void LoadData()
         {
             //считывает данные с файла и запичывает в лист
+        }
+
+        public void Shuffle(int index)
+        {
+            base.Insert(0, base[index]);
+            
+            for(int i = index + 1; i < 5; i++)
+            {
+                base[i] = base[i + 1];
+            }
+            base.RemoveAt(5);
         }
     }
 }
